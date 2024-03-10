894effcf3835f9cbefe1fa6f68fbf645c7859c37
ThijmenGThN <mail@thijmenheuvelink.nl>
2024-03-10 23:52:43 +0100

    💚 Health-check insights regarding API availability

---
+++ src/health.rs
@@ -0,0 +1,37 @@
+use reqwest::blocking as request;
+
+use crate::logger::alert;
+use crate::system::exit;
+use crate::system::Envs;
+
+pub fn check(env: &Envs) {
+    // Check if the API can be reached.
+    match request::get(&format!("{}/api/v3/health", &env.baseurl)) {
+        Ok(res) => {
+            if res.status() != 200 {
+                alert(
+                    "FATAL",
+                    format!("The provided \"APIKEY\" is not valid."),
+                    format!(
+                        "Obtain the {} API key in Settings > General > API Key",
+                        &env.platform
+                    ),
+                    true,
+                );
+                exit(1);
+            }
+        }
+        Err(error) => {
+            alert(
+                "FATAL",
+                format!(
+                    "A connection to the {} API could not be established.",
+                    &env.platform
+                ),
+                error.to_string(),
+                true,
+            );
+            exit(1);
+        }
+    }
+}
